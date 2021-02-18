1f981ecc470f7e23517d4f492d8b9d2935062ab2
gleam <nobody.roman@gmail.com>
2021-02-18 19:31:05 +0300

    initial commit

---
+++ initialize.sh
@@ -0,0 +1,19 @@
+echo Starter in progress... &&
+  echo preparing backend... &&
+  # @NOTE: for debug purpose...
+  # rm -rf "./backend" &&
+  # rm -rf "./frontend" &&
+  git clone --no-checkout https://github.com/web-bee-ru/koa-starter.git backend &&
+    cd backend &&
+    git checkout master &&
+    # cp .env.example .env &&
+    rm -rf .git &&
+    cd ../ &&
+  echo preparing frontend... &&
+  git clone --no-checkout https://github.com/web-bee-ru/nuxt-starter.git frontend &&
+    cd frontend &&
+    git checkout master &&
+    # cp .env.example .env &&
+    rm -rf .git &&
+    cd ../ &&
+  echo Starter complete!
