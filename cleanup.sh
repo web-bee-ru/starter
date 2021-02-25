70bc1f813d2f841bd45a72ec19500616e130d9db
gleam <nobody.roman@gmail.com>
2021-02-25 19:31:54 +0300

    chore: replacing README.md after install

--- cleanup.sh
+++ cleanup.sh
@@ -2,5 +2,6 @@ echo Cleanup in progress... &&
   rm -rf .git &&
   rm -rf *.sh &&
   mv package.json.default package.json &&
+  mv README.md.default README.md &&
   echo Starter cleanup complete! Your project is ready to commit. &&
   echo Dont forget to exec \"mv .env.example .env\" in \"./frontend\" and \"./backend\" folders
